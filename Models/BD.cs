e9653ece8b7b5c7dde32d81b09ed9fe06503c289
MATIAS IVAN SZABO <47805194@ort>
2023-11-02 13:46:13 -0300

    terminados los models

--- Models/BD.cs
+++ Models/BD.cs
@@ -20,34 +20,68 @@ namespace Prac.Models
             return ListaPaises;
         }
     
-       public static Viajes SeleccionarViajesPorPais(int id)
-    {
-        Viajes Elegido = null;
-        using (SqlConnection db = new SqlConnection(_connectionString))
+        public static Viajes SeleccionarViajesPorPais(int id)
         {
-            string SQL = "SELECT * FROM Viajes WHERE Viajes.ID_Pais= @ID_Pais";
-            Elegido = db.QueryFirstOrDefault<Viajes>(SQL, new { pid= id });
+            Viajes Elegido = null;
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string SQL = "SELECT * FROM Viajes WHERE Viajes.ID_Pais= @ID_Pais";
+                Elegido = db.QueryFirstOrDefault<Viajes>(SQL, new { pid= id });
+            }
+            return Elegido;
         }
-        return Elegido;
-    }
-public static void InsertViaje(Viaje viaje)
-{
-    string SQL = "INSERT INTO Viaje(ID_Viaje, nombre, precio, imagen, descripcion, puntaje, ID_Pais)";
-    SQL += " VALUES (@pID_Viaje, @pnombre, @pprecio, @pimagen, @pdescripcion, @ppuntaje, @pID_Pais)";
-
-    using (SqlConnection db = new SqlConnection(_connectionString))
-    {
-        db.Execute(SQL, new
+        public static void InsertViaje(Viaje viaje)
         {
-            pID_Viaje = viaje.ID_Viaje,
-            pnombre = viaje.nombre,
-            pprecio = viaje.precio,
-            pimagen = viaje.imagen
-            pdescripcion = viaje.descripcion
-            ppuntaje = viaje.puntaje
-            pID_Pais = viaje.ID_Pais
-        });
-    }
-}
+            string SQL = "sp_InsertViajes";
+            SQL += " VALUES (@pID_Viaje, @pnombre, @pprecio, @pimagen, @pdescripcion, @ppuntaje, @pID_Pais)";
+
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                db.Execute(SQL, new
+                {
+                    pID_Viaje = viaje.ID_Viaje,
+                    pnombre = viaje.nombre,
+                    pprecio = viaje.precio,
+                    pimagen = viaje.imagen,
+                    pdescripcion = viaje.descripcion,
+                    ppuntaje = viaje.puntaje,
+                    pID_Pais = viaje.ID_Pais
+                }
+                );
+            }
+        }
+        public static void InsertPais(Pais pais)
+        {
+            string SQL = "sp_InsertPais";
+            SQL += "VALUES(@pID_Pais, @pnombre, @pDescripcion)";
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                db.Execute(SQL, new
+                {
+                    pID_Pais = pais.ID_Pais,
+                    pnombre = pais.nombre,
+                    pDescripcion = pais.Descripcion
+                }
+                );
+            }
+        }
+        public static void InssertUsuario(Usuario usuario)
+        {
+            string SQL = "sp_InsertUsuario"
+            SQL += "VALUES(@pID_Usuario, @pnombre, @pemail, @pcontraseña, @pID_Viaje, @pFechaNacimiento)"
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                db.Execute(SQL, new
+                {
+                    pID_Usuario = usuario.ID_Usuario,
+                    pnombre = usuario.nombre,
+                    pemail = usuario.email,
+                    pcontraseña = usuario.contraseña,
+                    pID_Viaje = usuario.ID_Viaje,
+                    pFechaNacimiento = usuario.FechaNacimiento
+                }
+                );
+            }
+        }
     }
 }
